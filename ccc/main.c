80e6ec0ab116d1030a77dd82cf4978da8413b41f
SalmaOsama2004 <salmakhalil1243@gmail.com>
2024-07-26 19:12:14 +0300

    Add files via upload

---
+++ ccc/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A ,B;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    printf("A + B = "); printf("%d",A+B);
+    printf("\nA * B = "); printf("%d",A*B);
+    printf("\nA / B = "); printf("%d",A/B);
+    printf("\nA - B = "); printf("%d",A-B);
+
+
+    return 0;
+}
